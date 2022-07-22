a7a54030f2eb584aa4b8c6bc5c910c714da35c69
Сергей Кулаков <77716512sergey@gmail.com>
2022-07-22 16:22:36 +0300

    SpringHellowWorld

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,18 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        boolean comparison = bean == bean;
+        System.out.println(comparison);
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+        boolean comparisonCat = catBean == catBean1;
+        System.out.println(comparisonCat);
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String massage;
+
+    public String getMessage() {
+        return massage;
+    }
+
+    public void setMassage(String massage) {
+        this.massage = massage;
+    }
+}
